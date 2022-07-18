0d9bc9b8fd118e160f8fbed82c623390c04a9da5
yangyu3390 <yangyu3390@gmail.com>
2022-07-19 09:39:07 +1000

    authz scheme

---
+++ pkg/authz/authz.go
@@ -0,0 +1,64 @@
+package authz
+
+import (
+	"errors"
+	"strings"
+	"tcploadbalancer/config"
+
+	log "github.com/sirupsen/logrus"
+)
+
+// AuthzRule defines an authorization rule.
+type AuthzRule struct {
+	IsAllowed    bool
+	CommonName   string
+	UpstreamAddr string // ip:port
+}
+
+type AuthzScheme struct {
+	Rules []AuthzRule
+}
+
+func New(cfg config.AuthzCfg) (AuthzScheme, error) {
+
+	authzScheme := AuthzScheme{
+		Rules: make([]AuthzRule, 0),
+	}
+
+	for _, rule := range cfg.Rules {
+		// parse rules
+		parts := strings.Split(rule, "-")
+		if len(parts) != 3 {
+			log.Error("Bad authz rule format", rule)
+			return AuthzScheme{}, errors.New("Bad authz rule format: " + rule)
+		}
+
+		commonName := strings.TrimSpace(parts[0])
+		isAllowed := strings.TrimSpace(parts[1])
+		upstreamAddr := strings.TrimSpace(parts[2])
+
+		if isAllowed != "allow" && isAllowed != "deny" {
+			log.Error("Unsupported isAllow value: ", isAllowed)
+			return AuthzScheme{}, errors.New("Unsupported isAllow value: " + isAllowed)
+		}
+
+		rule := AuthzRule{
+			IsAllowed:    isAllowed == "allow",
+			CommonName:   commonName,
+			UpstreamAddr: upstreamAddr,
+		}
+
+		authzScheme.Rules = append(authzScheme.Rules, rule)
+	}
+	return authzScheme, nil
+}
+
+func (a *AuthzScheme) Allows(commonName string, upstreamAddr string) bool {
+	for _, r := range a.Rules {
+		if strings.Compare(r.CommonName, commonName) == 0 && strings.Compare(r.UpstreamAddr, upstreamAddr) == 0 {
+			return r.IsAllowed
+		}
+	}
+	// if no matches found, by default, allow access
+	return true
+}
---
+++ pkg/authz/authz_test.go
@@ -0,0 +1,75 @@
+package authz
+
+import (
+	"tcploadbalancer/config"
+	"testing"
+)
+
+func TestAuthZ(t *testing.T) {
+
+	tests := []struct {
+		description  string
+		clients      []string
+		rules        config.AuthzCfg
+		upstreamAddr string
+		want         []bool
+	}{
+		{
+			description: "happy path",
+			clients:     []string{"client a", "client b"},
+			rules: config.AuthzCfg{
+				Rules: []string{
+					"client a - allow - 127.0.0.1:8000",
+					"client b - deny - 127.0.0.1:8000",
+				},
+			},
+			upstreamAddr: "127.0.0.1:8000",
+			want:         []bool{true, false},
+		},
+		{
+			description: "empty rules. allow access",
+			clients:     []string{"client a", "client b"},
+			rules: config.AuthzCfg{
+				Rules: []string{},
+			},
+			upstreamAddr: "127.0.0.1:8000",
+			want:         []bool{true, true},
+		},
+		{
+			description: "if no rules matches, allow access by default",
+			clients:     []string{"client a", "client b", "client c"},
+			rules: config.AuthzCfg{
+				Rules: []string{
+					"client a - allow - 127.0.0.1:8000",
+					"client b - deny - 127.0.0.1:8000",
+				},
+			},
+			upstreamAddr: "127.0.0.1:8000",
+			want:         []bool{true, false, true},
+		},
+		{
+			description: "if multiple rules matches, select the first one",
+			clients:     []string{"client a", "client a"},
+			rules: config.AuthzCfg{
+				Rules: []string{
+					"client a - allow - 127.0.0.1:8000",
+					"client a - deny - 127.0.0.1:8000",
+				},
+			},
+			upstreamAddr: "127.0.0.1:8000",
+			want:         []bool{true, true},
+		},
+	}
+
+	for _, tc := range tests {
+		a, err := New(tc.rules)
+		if err != nil {
+			t.Errorf("%v, %v", tc.clients, tc.rules)
+		}
+		for i, client := range tc.clients {
+			if got := a.Allows(client, tc.upstreamAddr); got != tc.want[i] {
+				t.Errorf("%s, (%v) = %v", tc.description, got, tc.want[i])
+			}
+		}
+	}
+}
