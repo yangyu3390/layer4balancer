389f32f07f8a700ae887843146958a1c1c4fbb16
yangyu3390 <yangyu3390@gmail.com>
2022-07-23 17:10:13 +1000

    rate limiter

---
+++ pkg/ratelimit/ratelimit.go
@@ -0,0 +1,108 @@
+package ratelimit
+
+import (
+	"sync"
+	"tcploadbalancer/config"
+	"time"
+
+	log "github.com/sirupsen/logrus"
+)
+
+type client struct {
+	numReq int
+	// window index for the current request.
+	// This breaks down the time into discrete windows of RateLimiterCfg.Window width
+	windowIdx int
+	lastSeen  time.Time
+}
+
+type RateLimiter struct {
+	clients         map[string]*client
+	stop            chan bool
+	cleanupInterval time.Duration
+	limit           int
+	window          time.Duration
+	mu              sync.Mutex
+}
+
+func New(cfg config.RateLimiterCfg) *RateLimiter {
+
+	return &RateLimiter{
+		clients:         make(map[string]*client),
+		stop:            make(chan bool),
+		cleanupInterval: cfg.CleanupInterval,
+		limit:           cfg.Limit,
+		window:          cfg.Window,
+	}
+}
+
+func (r *RateLimiter) Start() {
+
+	ticker := time.NewTicker(r.cleanupInterval)
+
+	go func() {
+		for {
+			select {
+
+			// new check interval has reached
+			case <-ticker.C:
+				go r.cleanup()
+
+			// request to stop cleanup
+			case <-r.stop:
+				ticker.Stop()
+				log.Info("rate limiter stopped")
+				return
+			}
+		}
+	}()
+	log.Info("rate limiter started!")
+}
+
+func (r *RateLimiter) cleanup() {
+	r.mu.Lock()
+
+	for commonName, client := range r.clients {
+		if time.Since(client.lastSeen) > r.cleanupInterval {
+			delete(r.clients, commonName)
+		}
+	}
+	r.mu.Unlock()
+}
+
+func (r *RateLimiter) Stop() {
+	r.stop <- true
+}
+
+func (r *RateLimiter) Allows(clientId string) bool {
+
+	r.mu.Lock()
+	defer r.mu.Unlock()
+	currWindowIdx := int(time.Now().UnixMilli() / int64(r.window/time.Millisecond))
+
+	// new client
+	if _, found := r.clients[clientId]; !found {
+		r.clients[clientId] = &client{
+			numReq:    0,
+			windowIdx: currWindowIdx,
+			lastSeen:  time.Now(),
+		}
+	}
+
+	// if windowidx is smaller than the current window index, then we know it is not rate limited.
+	if r.clients[clientId].windowIdx < currWindowIdx {
+		r.clients[clientId].windowIdx = currWindowIdx
+		r.clients[clientId].numReq = 0
+		r.clients[clientId].lastSeen = time.Now()
+	} else {
+		r.clients[clientId].numReq++
+		r.clients[clientId].lastSeen = time.Now()
+	}
+
+	if r.clients[clientId].numReq > r.limit {
+		log.Info("Rate limiter: ", clientId, " is rate limited")
+		return false
+	}
+
+	return true
+}
---
+++ pkg/ratelimit/ratelimit_test.go
@@ -0,0 +1,122 @@
+package ratelimit
+
+import (
+	"tcploadbalancer/config"
+	"testing"
+	"time"
+)
+
+func TestRateLimit(t *testing.T) {
+
+	tests := []struct {
+		description string
+		config      config.RateLimiterCfg
+		clientReqs  []string
+		want        []bool
+	}{
+		{
+			description: "happy path",
+			config: config.RateLimiterCfg{
+				CleanupInterval: 60 * time.Second,
+				Limit:           1,               // max number of requests within the time window
+				Window:          1 * time.Second, // time window
+			},
+			clientReqs: []string{
+				"client a",
+				"client b",
+				"client a",
+				"client a",
+			},
+			want: []bool{true, true, false, false},
+		},
+		{
+			description: "reject all requests",
+			config: config.RateLimiterCfg{
+				CleanupInterval: 60 * time.Second,
+				Limit:           0,
+				Window:          1 * time.Second,
+			},
+			clientReqs: []string{
+				"client a",
+				"client b",
+				"client a",
+				"client a",
+			},
+			want: []bool{false, false, false, false},
+		},
+	}
+
+	for _, tc := range tests {
+		r := New(tc.config)
+
+		for i, req := range tc.clientReqs {
+			got := r.Allows(req)
+			if got != tc.want[i] {
+				t.Errorf("%s, %v != %v", tc.description, got, tc.want[i])
+			}
+		}
+
+	}
+}
+
+func TestCleanup(t *testing.T) {
+	tests := []struct {
+		description string
+		config      config.RateLimiterCfg
+		clientReqs  []string
+		sleep       time.Duration
+		want        []int
+	}{
+		{
+			description: "client a got deleted twice and client b got deleted once",
+			config: config.RateLimiterCfg{
+				CleanupInterval: 100 * time.Millisecond,
+				Limit:           1,
+				Window:          1 * time.Second,
+			},
+			clientReqs: []string{
+				"client a",
+				"client b",
+				"client a",
+				"client a",
+			},
+			sleep: 1 * time.Second,
+			// count the number of elements in the client map
+			want: []int{1, 1, 1, 1},
+		},
+		{
+			description: "no clients got deleted",
+			config: config.RateLimiterCfg{
+				CleanupInterval: 10 * time.Second,
+				Limit:           0,
+				Window:          1 * time.Second,
+			},
+			clientReqs: []string{
+				"client a",
+				"client b",
+				"client a",
+				"client a",
+			},
+			sleep: 1 * time.Second,
+			want:  []int{1, 2, 2, 2},
+		},
+	}
+
+	for _, tc := range tests {
+		r := New(tc.config)
+		r.Start()
+		for i, req := range tc.clientReqs {
+			r.Allows(req)
+			// prevent data race in test
+			r.mu.Lock()
+			got := len(r.clients)
+			if got != tc.want[i] {
+				r.mu.Unlock()
+				t.Errorf("%s, %v != %v", tc.description, got, tc.want[i])
+			}
+			r.mu.Unlock()
+			time.Sleep(tc.sleep)
+		}
+		r.Stop()
+	}
+}
